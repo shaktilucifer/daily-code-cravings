eba6f73900287fa167547fea0cd2676ad9c8d064
shakti lucifer <shakticse@gmail.com>
2017-05-01 23:21:32 -0400

    Add Node class

--- datastructures/ListLinked.java
+++ datastructures/ListLinked.java
@@ -1,21 +1,40 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-class ListLinked{
+
+class ListLinked {
     Node head;
     int size;
 
 
 }
 
-class Node{
+class Node {
+    private Object data;
+    private Node next;
+
+    Node() {
+        this.data = null;
+        this.next = null;
+    }
+
+    Node(Object data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return this.next;
+    }
+
+    public void setNext(Node node) {
+        this.next = node;
+    }
+
 
 }
 
-class ListLinkedTester
-{
-    public static void main (String[] args)
-    {
+class ListLinkedTester {
+    public static void main(String[] args) {
 
     }
 }
