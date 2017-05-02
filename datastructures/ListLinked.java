4066c8b40c43b2a33e43512862d9a6029ef16192
shakti lucifer <shakticse@gmail.com>
2017-05-01 23:08:56 -0400

    Linked list Skeleton

---
+++ datastructures/ListLinked.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class ListLinked{
+    Node head;
+    int size;
+
+
+}
+
+class Node{
+
+}
+
+class ListLinkedTester
+{
+    public static void main (String[] args)
+    {
+
+    }
+}
