ed337153580b9d594f086d98e336ab316c2fa817
shakti lucifer <shakticse@gmail.com>
2017-05-02 00:25:18 -0400

    Find operation for LinkedList

--- datastructures/ListLinked.java
+++ datastructures/ListLinked.java
@@ -1,74 +1,90 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-class ListLinked{
+
+class ListLinked {
     Node head;
     int size;
-    ListLinked(){
+
+    ListLinked() {
         head = new Node();
     }
+
     //Returns if the list is empty or not
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return head.getNext() == null;
     }
-    public int size(){
+
+    public int size() {
         return size;
     }
+
     /*
     * Adds an element to the end of the list
     */
-    public void addNode(Object data){
+    public void addNode(Object data) {
         Node nodeToAdd = new Node(data);
         Node currentNode = head;
-        while(currentNode.getNext() != null){
+        while (currentNode.getNext() != null) {
             currentNode = currentNode.getNext();
         }
         currentNode.setNext(nodeToAdd);
         this.size++;
     }
 
-    public void printList(){
-        if(this.isEmpty()){
+    public Node find(Object data) {
+        Node currentNode = head;
+        while (currentNode.getNext() != null) {
+            if (currentNode.getNext().data.equals(data)) {
+                return currentNode;
+            }
+            currentNode = currentNode.getNext();
+        }
+        return null;
+    }
+
+    public void printList() {
+        Node currentNode = head;
+        if (this.isEmpty()) {
             System.out.println("The List is empty!");
             return;
         }
-        while(head.getNext() != null){
-            System.out.println(head.getNext().data);
-            head = head.getNext();
+        while (currentNode.getNext() != null) {
+            System.out.println(currentNode.getNext().data);
+            currentNode = currentNode.getNext();
             System.out.println();
         }
     }
 
 }
 
-class Node{
+class Node {
     public Object data;
     private Node next;
-    Node(){
+
+    Node() {
         this.data = null;
         this.next = null;
     }
-    Node(Object data){
+
+    Node(Object data) {
         this.data = data;
         this.next = null;
     }
 
-    public Node getNext(){
+    public Node getNext() {
         return this.next;
     }
 
-    public void setNext(Node node){
+    public void setNext(Node node) {
         this.next = node;
     }
 
 
-
 }
 
-class ListLinkedTester
-{
-    public static void main (String[] args)
-    {
+class ListLinkedTester {
+    public static void main(String[] args) {
         ListLinked list = new ListLinked();
         System.out.println(list.isEmpty());
         list.addNode("testString");
@@ -77,5 +93,13 @@ class ListLinkedTester
         System.out.println(list.isEmpty());
         list.printList();
         System.out.println(list.size());
+        System.out.println(list.isEmpty());
+        //Test for finding an element in the list
+        Node node = list.find(2.2);
+        if (node == null) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element is found");
+        }
     }
 }
