e91f0f72b3e31eedd440cb9d70446ac05ebbbbc1
Shakti <shakticse@gmail.com>
2017-05-03 22:35:49 -0400

    Skeleton for queue

---
+++ datastructures/Queue.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Queue {
+	private Node head;
+	private Node tail;
+	Queue(){
+		
+	}
+	
+	public void enqueue(){
+		
+	}
+	
+	public void dequeue(){
+		
+	}
+	
+}
+
+class Node{
+	private Node next;
+	private Object item;
+}
+
+/**
+ * Testing Class
+ */ 
+class QueueTester {
+    public static void main(String[] args) {
+       
+    }
+}
