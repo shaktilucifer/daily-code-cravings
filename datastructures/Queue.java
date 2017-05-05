ffb36da015b88ed048f170b549ce505639ab0f6b
Shakti <shakticse@gmail.com>
2017-05-04 22:50:20 -0400

    Add node class

--- datastructures/Queue.java
+++ datastructures/Queue.java
@@ -8,11 +8,12 @@ class Queue {
 	private int size;
 	
 	Queue(){
-		
+		head = null;
+		tail = head;
 	}
 	
 	public void enqueue(Object item){
-		
+	
 	}
 	
 	public void dequeue(){
@@ -21,6 +22,8 @@ class Queue {
 	
 	public Node peek(){
 		
+		//Peek for node 
+		return tail;
 	}
 	
 	
@@ -29,6 +32,27 @@ class Queue {
 class Node{
 	private Node next;
 	private Object item;
+	
+	Node(Object item){
+		this.next = null;
+		this.item = item;
+	}
+	
+	public Node getNext(){
+		return this.next;
+	}
+	
+	public void setNext(Node node){
+		this.next = node;
+	}
+	
+	public Object getItem(){
+		return this.item;
+	}
+	
+	public void setItem(Object item){
+		this.item = item;
+	}
 }
 
 /**
