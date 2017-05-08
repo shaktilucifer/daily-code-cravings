0b7ba4a4daf60fcc7aadcf97438a0ef3e840620f
Shakti <shakticse@gmail.com>
2017-05-07 20:57:46 -0400

    stack.java skeleton

---
+++ datastructures/Stack.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Stack
+{
+	private Node top;
+	private int size;
+	
+	
+
+
+}
+
+class Node{
+	Node(){
+		
+	}
+}
+
+class StackTester{
+		
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+}
+}
