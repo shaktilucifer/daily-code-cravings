c9abe3ed0ceda202759db638494db87dc1a44c81
Shakti <shakticse@gmail.com>
2017-05-16 20:58:26 -0400

    Stackarray skeleton

---
+++ datastructures/StackArray.java
@@ -0,0 +1,38 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class StackArray{
+
+    private Object[] stack;
+    private int size;
+    
+    StackArray(){
+        this.stack = new Object[100];
+    }
+    
+    public StackArray push(){
+        
+        return null;
+    }
+    
+    public Object pop(){
+        
+        return null;
+    }
+    
+    public int size(){
+        
+    }
+    
+		
+}
+
+class StackArrayTester
+{
+	
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	}
+}
