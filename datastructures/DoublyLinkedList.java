8c830d6cb288bd6b07b81f2a75fae6eb4acd87f2
Shakti <shakticse@gmail.com>
2017-05-19 10:59:45 -0400

    append

--- datastructures/DoublyLinkedList.java
+++ datastructures/DoublyLinkedList.java
@@ -2,7 +2,26 @@ class DoublyLinkedList{
 	private Node head;
 	private int size;
 
-	public DoublyLinkedList append(){
+	DoublyLinkedList(){
+		this.head = new Node();
+		this.size = 0;
+	}
+
+	public DoublyLinkedList append(Object data){
+			Node node = new Node(data);
+			Node temp = head;
+			if(temp.getNext() == null){
+				temp.setNext(node);
+				node.setPrev(temp);
+				size++;
+				return this;
+			}
+			while(temp.getNext() != null){
+				temp = temp.getNext();
+			}
+			temp.setNext(node);
+			node.setPrev(temp);
+			size++;
 			return this;
 	}
 
@@ -42,6 +61,17 @@ class DoublyLinkedList{
 
 	}
 
+	public int size(){
+		return size;
+	}
+
+	public void printList(){
+		Node temp = head;
+		while(temp.getNext() != null){
+			temp = temp.getNext();
+			temp.printNode();
+		}
+	}
 }
 
 class Node{
@@ -49,12 +79,28 @@ class Node{
 	private Node next;
 	private Node prev;
 
+	Node(){
+		this.data = null;
+	}
+
+	Node(Object data){
+		this.data = data;
+	}
+
 	public void setNext(Node node){
 		this.next = node;
 	}
 
-	public Node getNext(Node node){
+	public Node getNext(){
 		return this.next;
+	}
+
+	public void setPrev(Node node){
+		this.prev = node;
+	}
+
+	public Node getPrev(){
+		return this.prev;
 	}
 
 	public Object getData(){
@@ -65,8 +111,8 @@ class Node{
 		this.data = data;
 	}
 
-	public void printNode(Node node){
-		System.out.println(node.getData());
+	public void printNode(){
+		System.out.println(getData());
 	}
 }
 
