d644049b9ade9abbb509f2edddadcf1f68295ab9
Shakti <shakticse@gmail.com>
2017-05-25 23:45:18 -0400

    Diff the sum

--- puzzles/PairSum.java
+++ puzzles/PairSum.java
@@ -11,8 +11,29 @@
 // OR
 // Pair found at index 1 and 4 (7 + 3)
 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 class PairSum{
-
+	class Pair{
+		int index1;
+		int index2;
+	}
+	
+	public PairSum findPair(int[] array,int sum){
+		HashMap<Integer,Integer> diffMap = new HashMap<Integer,Integer>();
+		for(int i=0;i<array.length() -1;i++){
+			int diff = sum - array[i];
+			if(diffMap.containsKey(array[i])){
+			System.out.println(diffMap.get(array[i]));
+			System.out.println(i);
+			}else{
+				diffMap.put(diff,i);
+			}
+		}
+		
+	}
+	
 
 	public static void main(String[] args){
 
