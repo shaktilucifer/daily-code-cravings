752c7adabd991cc2ebfad25951e8f42bd362b733
Shakti <shakticse@gmail.com>
2017-05-27 23:03:27 -0400

    Basic Node for Binary tree

--- datastructures/BinaryTree.java
+++ datastructures/BinaryTree.java
@@ -1,10 +1,16 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
 class BinaryTree
 {
+	private Node root;
 	
+	class Node{
+		private Node right;
+		private Node left;
+		private Node data;
+		
+		public void printNode(){
+			System.out.println("The value of the node is "+ this.data);
+		}
+	}
 	
 	public static void main (String[] args) throws java.lang.Exception
 	{
