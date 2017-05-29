40b4af176cd53f8b522729023690564ddd5f9476
Shakti <shakticse@gmail.com>
2017-05-28 22:57:09 -0400

    Create fundamentals.cpp

---
+++ Learning CPP/fundamentals.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {{{
+	
+	std::cout << "Hello, World" << std::endl;
+	std::cout << "Hello,"<< std::endl;
+	std::cout << "World" << std::endl;
+	std::cout << 3 + 4 << std::endl;
+	/*  Tsting comments within */
+	std::cout << "This is a quote (\"),and this is a backslash (\\)";
+	return 0;
+}}}
