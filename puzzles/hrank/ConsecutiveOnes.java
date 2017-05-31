fb1a4ddada9daedc882db0956d616554e67bfadf
Shakti <shakticse@gmail.com>
2017-05-30 23:07:13 -0400

    Create ConsecutiveOnes.java

---
+++ puzzles/hrank/ConsecutiveOnes.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class ConsecutiveOnes {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int max = 0;
+        int reminder = 0;
+        int onesCounter = 0;
+        while(n > 0){
+            reminder = n % 2;
+            if(reminder == 1){
+                onesCounter++;
+            }else{ 
+                if(onesCounter > max){
+                    max = onesCounter;
+                }
+                    onesCounter = 0;
+            }
+            n = n / 2;         
+        }
+        System.out.println(max > onesCounter ? max : onesCounter);
+    }
+}
