a97792fc6f9f700a66023b0338a2e8ff6b08d486
Shakti <shakticse@gmail.com>
2017-06-04 23:32:17 -0400

    Sample I/O

--- puzzles/hrank/ReverseArray.java
+++ puzzles/hrank/ReverseArray.java
@@ -1,5 +1,13 @@
 // Task 
 // Given an array,A , of N integers, print A's elements in reverse order as a single line of space-separated numbers.
+
+// Sample Input
+
+// 4
+// 1 4 3 2
+// Sample Output
+
+2 3 4 1
 import java.io.*;
 import java.util.*;
 
