94d010205c03f1fdddf0fa416088e8c2d1c02c0d
Shakti <shakticse@gmail.com>
2017-06-08 23:05:20 -0400

    Create D12Inheritance.java

---
+++ puzzles/hrank/D12Inheritance.java
@@ -0,0 +1,51 @@
+class Student extends Person{
+	private int[] testScores;  
+  
+    /*	
+    *   Class Constructor
+    *   
+    *   Parameters: 
+    *   firstName - A string denoting the Person's first name.
+    *   lastName - A string denoting the Person's last name.
+    *   id - An integer denoting the Person's ID number.
+    *   scores - An array of integers denoting the Person's test scores.
+    */
+    Student(String firstName, String lastName, int id, int[] scores){
+        super(firstName,lastName,id);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = id;
+        this.testScores = scores;
+    }
+    /*	
+    *   Method Name: Calculate
+    *   Return: A character denoting the grade.
+    */
+    public char calculate(){
+        float average = 0;
+        for(int i=0; i<testScores.length; i++){
+            average += testScores[i];
+        }
+        average = average / testScores.length; 
+        
+        if(average >= 90 && average <= 100){
+            return 'O';
+        }
+        else if(average >= 80 && average < 90){
+            return 'E';
+        }
+        else if(average >= 70 && average < 80){
+            return 'A';
+        }
+        else if(average >= 55 && average < 70){
+            return 'P';
+        }
+        else if(average >= 40 && average < 55){
+            return 'D';
+        }
+        else if(average < 40){
+            return 'T';
+        }
+        return 'F';
+    }
+}
