083cc57b6a7616e893f84767787ec7da092415c0
Shakti <shakticse@gmail.com>
2017-06-12 21:58:23 -0400

    Create ListDeleteDuplicate.java

---
+++ puzzles/hrank/ListDeleteDuplicate.java
@@ -0,0 +1,12 @@
+    public static Node removeDuplicates(Node head) {
+      Node temp = head;
+      while(temp != null && temp.next != null){
+          if(temp.data == temp.next.data){
+              temp.next = temp.next.next;
+          }else{
+            temp = temp.next;    
+          }
+     }  
+      return head;  
+
+    }
