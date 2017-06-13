bdea2e70c8e9382eb9e2ef64ac6f753ea73bacd4
Shakti <shakticse@gmail.com>
2017-06-12 22:03:13 -0400

    Update ListDeleteDuplicate.java

--- puzzles/hrank/ListDeleteDuplicate.java
+++ puzzles/hrank/ListDeleteDuplicate.java
@@ -12,67 +12,62 @@
 // 1 2 3 4 
 import java.io.*;
 import java.util.*;
-class Node{
-	int data;
-	Node next;
-	Node(int d){
-        data=d;
-        next=null;
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
     }
-	
+
 }
-class ListDeleteDuplicate{
-	
+class ListDeleteDuplicate {
+
     public static Node removeDuplicates(Node head) {
-      Node temp = head;
-      while(temp != null && temp.next != null){
-          if(temp.data == temp.next.data){
-              temp.next = temp.next.next;
-          }else{
-            temp = temp.next;    
-          }
-     }  
-      return head;  
+        Node temp = head;
+        while (temp != null && temp.next != null) {
+            if (temp.data == temp.next.data) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+        return head;
 
     }
-    
-     public static  Node insert(Node head,int data)
-    {
-        Node p=new Node(data);			
-        if(head==null)
-            head=p;
-        else if(head.next==null)
-            head.next=p;
-        else
-        {
-            Node start=head;
-            while(start.next!=null)
-                start=start.next;
-            start.next=p;
+
+    public static Node insert(Node head, int data) {
+        Node p = new Node(data);
+        if (head == null)
+            head = p;
+        else if (head.next == null)
+            head.next = p;
+        else {
+            Node start = head;
+            while (start.next != null)
+                start = start.next;
+            start.next = p;
 
         }
         return head;
     }
-    public static void display(Node head)
-        {
-              Node start=head;
-              while(start!=null)
-              {
-                  System.out.print(start.data+" ");
-                  start=start.next;
-              }
+    public static void display(Node head) {
+        Node start = head;
+        while (start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
         }
-        public static void main(String args[])
-        {
-              Scanner sc=new Scanner(System.in);
-              Node head=null;
-              int T=sc.nextInt();
-              while(T-->0){
-                  int ele=sc.nextInt();
-                  head=insert(head,ele);
-              }
-              head=removeDuplicates(head);
-              display(head);
-
-       }
     }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head, ele);
+        }
+        head = removeDuplicates(head);
+        display(head);
+
+    }
+}
