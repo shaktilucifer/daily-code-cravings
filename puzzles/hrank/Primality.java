5027a0cf56711a16ef88fc7ade872c3171d37226
Shakti <shakticse@gmail.com>
2017-06-13 21:04:59 -0400

    Create Primality.java

---
+++ puzzles/hrank/Primality.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Primality {
+    
+    public void printPrimality(int checkPrime){
+            if(checkPrime == 1){
+                System.out.println("Not prime");
+                return;
+            }
+            int squareRoot = (int)Math.sqrt(checkPrime);
+            
+            for(int j = 2; j <= squareRoot; j++){
+                if(checkPrime % j == 0){
+                    System.out.println("Not prime");
+                    return;
+                }
+            }
+            System.out.println("Prime");
+            return;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int samples = sc.nextInt();
+        Primality p = new Primality();  
+        for(int i=0; i < samples; i++ ){
+           p.printPrimality(sc.nextInt());
+        }
+    
+    }
+}
