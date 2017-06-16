e41ddd0a817bafd3ae3b623a211dac1bc8f0c04c
Shakti <shakticse@gmail.com>
2017-06-15 23:19:43 -0400

    Create BasicOop.java

---
+++ puzzles/hrank/BasicOop.java
@@ -0,0 +1,32 @@
+public class Person {
+    private int age;	
+  
+	public Person(int initialAge) {
+  		// Add some more code to run some checks on initialAge
+        if(initialAge < 0){
+            
+       this.age = 0;  
+       this.printStatement("Age is not valid, setting age to 0.");
+        }else{
+            this.age = initialAge;
+        }
+	}
+
+	public void amIOld() {
+  		// Write code determining if this person's age is old and print the correct statement:
+        if(this.age < 13) 
+            this.printStatement("You are young.");
+        else if(this.age >= 13 && this.age < 18)
+            this.printStatement("You are a teenager.");
+        else
+            this.printStatement("You are old.");
+    }
+    
+    public void printStatement(String toBePrinted){
+                System.out.println(toBePrinted);
+    }
+
+	public void yearPasses() {
+  		// Increment this person's age.
+        this.age += 1;
+	}
