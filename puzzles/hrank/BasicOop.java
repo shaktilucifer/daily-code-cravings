011b9d6cb8cc62f8b257c152ebcfd4239e28c57d
Shakti <shakticse@gmail.com>
2017-06-15 23:20:23 -0400

    Sample Input / Output

--- puzzles/hrank/BasicOop.java
+++ puzzles/hrank/BasicOop.java
@@ -30,3 +30,26 @@ public class Person {
   		// Increment this person's age.
         this.age += 1;
 	}
+
+	
+//Sample Input
+
+// 4
+// -1
+// 10
+// 16
+// 18
+// Sample Output
+
+// Age is not valid, setting age to 0.
+// You are young.
+// You are young.
+
+// You are young.
+// You are a teenager.
+
+// You are a teenager.
+// You are old.
+
+// You are old.
+// You are old.
