a3ff6f72861989c6434c6c3b42c03229f0c5f0f0
Shakti <shakticse@gmail.com>
2017-06-19 22:44:30 -0400

    Input - output test case

--- puzzles/hrank_algo/DiagonalDifference.java
+++ puzzles/hrank_algo/DiagonalDifference.java
@@ -24,3 +24,12 @@ public class DiagonalDifference {
         System.out.println(Math.abs(sumDiag1 - sumDiag2));
     }
 }
+// Sample Input
+
+// 3
+// 11 2 4
+// 4 5 6
+// 10 8 -12
+// Sample Output
+
+// 15
