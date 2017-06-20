4e0cbd963fb264939a05c47673ccfbdf5e77b239
Shakti <shakticse@gmail.com>
2017-06-19 22:42:47 -0400

    Create DiagonalDifference.java

---
+++ puzzles/hrank_algo/DiagonalDifference.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class DiagonalDifference {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a[][] = new int[n][n];
+        for(int a_i=0; a_i < n; a_i++){
+            for(int a_j=0; a_j < n; a_j++){
+                a[a_i][a_j] = in.nextInt();
+            }
+        }
+        int sumDiag1 = 0;
+        int sumDiag2 = 0;
+        for(int i=0; i < n; i++){
+            sumDiag1 += a[i][i];
+            sumDiag2 += a[i][(n-1) - i];
+        }
+        System.out.println(Math.abs(sumDiag1 - sumDiag2));
+    }
+}
