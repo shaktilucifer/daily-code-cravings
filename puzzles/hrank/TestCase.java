1e818bd2d8bdff872b6a3ceaa27d4d9dab402f45
Shakti <shakticse@gmail.com>
2017-06-21 22:10:53 -0400

    Fix based on question reread

--- puzzles/hrank/TestCase.java
+++ puzzles/hrank/TestCase.java
@@ -5,16 +5,21 @@ public class TestCase {
 
     public static void main(String[] args) {
         System.out.println("5");
-        System.out.println("4 1");
-        System.out.println("-15 -9999 11 0");
+        
         System.out.println("5 4");
         System.out.println("-1 0 1 1 -99999");
+        
+        System.out.println("4 1");
+        System.out.println("-15 -9999 11 0");
+      
+        System.out.println("6 3");
+        System.out.println("-1 0 2 1 212 223");
+        
         System.out.println("10 5");
         System.out.println("0 0 0 0 0 0 0 0 -1 1");
-        System.out.println("6 3");
-        System.out.println("-1 0 0 0 212 223");
+        
         System.out.println("8 7");
-        System.out.println("0 0 0 0 0 -999 1 -1");
+        System.out.println("0 10 10 10 10 -999 1 -1");
 
     
     }
