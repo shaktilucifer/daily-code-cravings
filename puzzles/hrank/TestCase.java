edca596a798f0778212eed8a218537d760b72f2e
Shakti <shakticse@gmail.com>
2017-06-21 20:31:27 -0400

    Create TestCase.java

---
+++ puzzles/hrank/TestCase.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class TestCase {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("5");
+        System.out.println("4 1");
+        System.out.println("-15 -9999 11 0");
+        System.out.println("5 4");
+        System.out.println("-1 0 1 1 -99999");
+        System.out.println("10 5");
+        System.out.println("0 0 0 0 0 0 0 0 -1 1");
+        System.out.println("6 3");
+        System.out.println("-1 0 0 0 212 223");
+        System.out.println("8 7");
+        System.out.println("0 0 0 0 0 -999 1 -1");
+
+    
+    }
+}
