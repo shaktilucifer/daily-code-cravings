d47403c5e817cf36306e5962c0d3df27a46a52e8
Shakti <shakticse@gmail.com>
2017-06-22 23:30:41 -0400

    Prettify

--- puzzles/InsertionBinary.java
+++ puzzles/InsertionBinary.java
@@ -8,30 +8,37 @@ import java.io.*;
  * It returns the same index if the value is already present.
  */
 
-class InsertionBinary
-{
-	public int findInsertionPoint(int[] sortedArr,float value){
-		int low = 0;
-		int high = sortedArr.length;
-		while(low < high){
-			int mid = (low + high) / 2;
-			if(sortedArr[mid] == value){
-				return mid;
-			}
-			if(sortedArr[mid] > value){
-				high = mid;
-			}else{
-				low = mid + 1;
-			}
-		}
-		return low;
-	}
-	
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		InsertionBinary i = new InsertionBinary();
-	int index =	i.findInsertionPoint(new int[]{1,2,3,4,5,6,7,9},9);
-	System.out.println(index);
-		
-	}
+class InsertionBinary {
+    public int findInsertionPoint(int[] sortedArr, float value) {
+        int low = 0;
+        int high = sortedArr.length;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (sortedArr[mid] == value) {
+                return mid;
+            }
+            if (sortedArr[mid] > value) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) throws java.lang.Exception {
+        InsertionBinary i = new InsertionBinary();
+        int index = i.findInsertionPoint(new int[] {
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            9
+        }, 9);
+        System.out.println(index);
+
+    }
 }
