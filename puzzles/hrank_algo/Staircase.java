cdad13e741724c8ba6e36f95f0f46a0539ea5acb
Shakti <shakticse@gmail.com>
2017-06-25 22:46:58 -0400

    Sample input output

--- puzzles/hrank_algo/Staircase.java
+++ puzzles/hrank_algo/Staircase.java
@@ -1,1 +1,15 @@
 
+// Sample Input
+
+// 6 
+// Sample Output
+
+//      #
+//     ##
+//    ###
+//   ####
+//  #####
+// ######
+// Explanation
+
+// The staircase is right-aligned, composed of # symbols and spaces, and has a height and width of .
