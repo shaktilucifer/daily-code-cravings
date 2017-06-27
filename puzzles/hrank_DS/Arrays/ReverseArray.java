1806ca6ee6aa62c2d9b2900ffac23abf4790a21b
Shakti <shakticse@gmail.com>
2017-06-26 23:04:25 -0400

    Modularize

--- puzzles/hrank_DS/Arrays/ReverseArray.java
+++ puzzles/hrank_DS/Arrays/ReverseArray.java
@@ -5,6 +5,13 @@ import java.math.*;
 import java.util.regex.*;
 
 public class Solution {
+    
+    public void printReverse(int[] arr,int n){        
+        //Printing in reverse
+         for(int i=n-1; i >=0; i--){
+            System.out.print(arr[i]+" ");
+        }
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
@@ -12,11 +19,8 @@ public class Solution {
         for(int arr_i=0; arr_i < n; arr_i++){
             arr[arr_i] = in.nextInt();
         }
-        
-        //Printing in reverse
-         for(int i=n-1; i >=0; i--){
-            System.out.print(arr[i]+" ");
-        }
+        Solution s= new Solution();
+        s.printReverse(arr,n);
     }
 }
 // Sample Input
