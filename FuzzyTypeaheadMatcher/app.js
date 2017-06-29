0c648ae8b097ebe190113a2b126320f1d32c2beb
Shakti <shakticse@gmail.com>
2017-06-28 23:02:40 -0400

    prettify

--- FuzzyTypeaheadMatcher/app.js
+++ FuzzyTypeaheadMatcher/app.js
@@ -1,30 +1,38 @@
+$(document).ready(function() {
 
-$(document).ready(function(){
+    var veggies = [
+        "Kidney Beans",
+        "Gourd",
+        "Drumstick",
+        "onions",
+        "Peas",
+        "Zucchini",
+        "okra"
+    ];
 
-var veggies = [
-    "Kidney Beans",
-"Gourd",
-"Drumstick",
-"onions",
-"Peas",
-"Zucchini",
-"okra"
-];
-
-var books = [{"title":"Pancreas", "author":"John Doe"}, {"title":"breasts", "author":"..."}];
-var fuzzyhound = new FuzzySearch({source:veggies});
-$('#typeahead-input').typeahead({
-            minLength: 1,
-            highlight: false //let FuzzySearch handle highlight
-        },
-        {
-            name: 'veggie',
-            source: fuzzyhound,
-            templates: {
-                suggestion: function(result){return "<div>"+fuzzyhound.highlight(result)+"</div>"}
+    var books = [{
+        "title": "Pancreas",
+        "author": "John Doe"
+    }, {
+        "title": "breasts",
+        "author": "..."
+    }];
+    var fuzzyhound = new FuzzySearch({
+        source: veggies
+    });
+    $('#typeahead-input').typeahead({
+        minLength: 1,
+        highlight: false //let FuzzySearch handle highlight
+    }, {
+        name: 'veggie',
+        source: fuzzyhound,
+        templates: {
+            suggestion: function(result) {
+                return "<div>" + fuzzyhound.highlight(result) + "</div>"
             }
-        }).bind('typeahead:change', function (obj, datum) {
-                console.log("changed value"+datum);
+        }
+    }).bind('typeahead:change', function(obj, datum) {
+        console.log("changed value" + datum);
     });
 
 
