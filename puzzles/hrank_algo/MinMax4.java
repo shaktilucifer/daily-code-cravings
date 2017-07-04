e5d10e17e4c112f650214e63baafc8f6cb5dfca5
Shakti <shakticse@gmail.com>
2017-07-03 23:19:49 -0400

    first method

--- puzzles/hrank_algo/MinMax4.java
+++ puzzles/hrank_algo/MinMax4.java
@@ -1,26 +1,34 @@
 import java.io.*;
+import java.util.*;
 
-public class MinMax4 {
+class MinMax {
+ public void printMinMax(int[] arr) {
+  Arrays.sort(arr);
+  long min = 0;
+  long max = 0;
+  for (int i = 0; i < 5; i++) {
+   if (i > 0) {
+    max += arr[i];
+   }
+   if (i < 4) {
+    min += arr[i];
+   }
+  }
+  System.out.print(min + " " + max);
+ }
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int[] arr = new int[5];
-        for(int arr_i=0; arr_i < 5; arr_i++){
-            arr[arr_i] = in.nextInt();
-        }
-        Arrays.sort(arr);
-        long min = 0;
-        long max = 0;
-        for(int i=0;i < 5; i++){
-            if(i > 0){
-                max += arr[i];
-            }
-            if(i < 4){
-                min += arr[i];
-            }
-        }
-        System.out.print(min +" "+max);
-    }
+ public static void main(String[] args) {
+  Scanner in = new Scanner(System.in);
+  int[] arr = new int[] {
+   1,
+   2,
+   3,
+   4,
+   5
+  };
+  MinMax mm = new MinMax();
+  mm.printMinMax(arr);
+ }
 }
 // Sample Input
 
