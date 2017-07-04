1b8e24ca4ee372e963eef6515f0c4479a6997b4f
Shakti <shakticse@gmail.com>
2017-07-03 23:25:59 -0400

    Faster - O(nlogn) to o(n)

--- puzzles/hrank_algo/MinMax4.java
+++ puzzles/hrank_algo/MinMax4.java
@@ -2,33 +2,51 @@ import java.io.*;
 import java.util.*;
 
 class MinMax {
- public void printMinMax(int[] arr) {
-  Arrays.sort(arr);
-  long min = 0;
-  long max = 0;
-  for (int i = 0; i < 5; i++) {
-   if (i > 0) {
-    max += arr[i];
-   }
-   if (i < 4) {
-    min += arr[i];
-   }
-  }
-  System.out.print(min + " " + max);
- }
+    public void printMinMax(int[] arr) {
+        Arrays.sort(arr);
+        long min = 0;
+        long max = 0;
+        for (int i = 0; i < 5; i++) {
+            if (i > 0) {
+                max += arr[i];
+            }
+            if (i < 4) {
+                min += arr[i];
+            }
+        }
+        System.out.print(min + " " + max);
+    }
+    
+    public void printMinMaxFast(int[] arr){
+    	long max = 0;
+    	long min = 999999;
+    	long sum = 0;
+    	for(int i=0; i < 5; i++){
+    		sum += arr[i];
+    		if(max < arr[i]){
+    			max = arr[i];
+    		}
+    		if(min > arr[i]){
+    			min = arr[i];
+    		}
+    	}
+    	System.out.println((sum-max)+" "+(sum-min));
+    }
 
- public static void main(String[] args) {
-  Scanner in = new Scanner(System.in);
-  int[] arr = new int[] {
-   1,
-   2,
-   3,
-   4,
-   5
-  };
-  MinMax mm = new MinMax();
-  mm.printMinMax(arr);
- }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[] {
+            1,
+            2,
+            3,
+            4,
+            5
+        };
+        MinMax mm = new MinMax();
+        mm.printMinMax(arr);
+        System.out.println();
+        mm.printMinMaxFast(arr);
+    }
 }
 // Sample Input
 
