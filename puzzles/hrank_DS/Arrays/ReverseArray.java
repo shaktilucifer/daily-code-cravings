2fabaf6c3e2331bf20f4526ea5cbb8d587184a1a
Shakti <shakticse@gmail.com>
2017-07-07 23:37:09 -0400

    Test cases and edge case

--- puzzles/hrank_DS/Arrays/ReverseArray.java
+++ puzzles/hrank_DS/Arrays/ReverseArray.java
@@ -11,10 +11,15 @@ class Solution {
          for(int i=n-1; i >=0; i--){
             System.out.print(arr[i]+" ");
         }
+        System.out.println();
     }
     public static void main(String[] args) {
        int[] arr = new int[]{12,3,4,5,22};
         Solution s= new Solution();
+        s.printReverse(arr,arr.length);
+        arr = new int[]{1,4,3,2};
+        s.printReverse(arr,arr.length);
+        arr = new int[]{-1,-1,-1,-1};
         s.printReverse(arr,arr.length);
     }
 }
