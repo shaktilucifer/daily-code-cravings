2b2263177b9841d30a13d6a02db4316f95116ec0
Shakti <shakticse@gmail.com>
2017-07-07 23:35:03 -0400

    basic input

--- puzzles/hrank_DS/Arrays/ReverseArray.java
+++ puzzles/hrank_DS/Arrays/ReverseArray.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+class Solution {
     
     public void printReverse(int[] arr,int n){        
         //Printing in reverse
@@ -13,14 +13,9 @@ public class Solution {
         }
     }
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int arr[] = new int[n];
-        for(int arr_i=0; arr_i < n; arr_i++){
-            arr[arr_i] = in.nextInt();
-        }
+       int[] arr = new int[]{12,3,4,5,22};
         Solution s= new Solution();
-        s.printReverse(arr,n);
+        s.printReverse(arr,arr.length);
     }
 }
 // Sample Input
