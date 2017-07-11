1d8ac09a0f26f46b1613c05d7640b470107e961e
Shakti <shakticse@gmail.com>
2017-07-10 23:21:24 -0400

    update test case and test case

--- puzzles/hrank_algo/ArraySum.java
+++ puzzles/hrank_algo/ArraySum.java
@@ -19,13 +19,10 @@ public class ArraySum {
     }
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] ar = new int[n];
-        for(int ar_i = 0; ar_i < n; ar_i++){
-            ar[ar_i] = in.nextInt();
-        }
-        int result = simpleArraySum(n, ar);
+        int[] ar = new int[]{1,2,3,4,10,11};
+        int result = simpleArraySum(ar.length, ar);
         System.out.println(result);
+        ar = new int[]{-1,-1,-1,-1,-1};
+        System.out.println(simpleArraySum(ar.length,ar));
     }
 }
