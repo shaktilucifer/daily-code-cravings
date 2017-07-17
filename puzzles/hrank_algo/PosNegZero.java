bb3cba240f5e1cd34d0c830d6abe139021c4d093
Shakti <shakticse@gmail.com>
2017-07-16 23:49:45 -0400

    hardcode testcase

--- puzzles/hrank_algo/PosNegZero.java
+++ puzzles/hrank_algo/PosNegZero.java
@@ -7,12 +7,8 @@ import java.util.regex.*;
 public class PosNegZero {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int arr[] = new int[n];
-        for(int arr_i=0; arr_i < n; arr_i++){
-            arr[arr_i] = in.nextInt();
-        }
+        int arr[] = new int[]{-4,3,-9,0,4,1};
+        int n = arr.length;
         double zeroes=0.0,pos=0.0,neg = 0.0;
         for(int i=0; i < n; i++){
            double total = arr[i] > 0 ? pos++ : arr[i] < 0 ? neg++ : zeroes++;
