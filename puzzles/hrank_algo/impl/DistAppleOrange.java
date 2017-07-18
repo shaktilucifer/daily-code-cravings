8ab2ac46f69877cebc66350c942940f86c696d54
Shakti <shakticse@gmail.com>
2017-07-17 22:24:39 -0400

    modular

--- puzzles/hrank_algo/impl/DistAppleOrange.java
+++ puzzles/hrank_algo/impl/DistAppleOrange.java
@@ -1,24 +1,8 @@
 import java.io.*;
 
 public class DistAppleOrange {
-
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int s = in.nextInt();
-        int t = in.nextInt();
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int m = in.nextInt();
-        int n = in.nextInt();
-        int[] apple = new int[m];
-        for(int apple_i=0; apple_i < m; apple_i++){
-            apple[apple_i] = in.nextInt();
-        }
-        int[] orange = new int[n];
-        for(int orange_i=0; orange_i < n; orange_i++){
-            orange[orange_i] = in.nextInt();
-        }
-        
+    public static void printNumFruits(int[] apple, int[] orange){
+          
         int appleCounter = 0;
         int orangeCounter = 0;
         
@@ -36,6 +20,25 @@ public class DistAppleOrange {
         }
         System.out.println(appleCounter);
         System.out.println(orangeCounter);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        int t = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int m = in.nextInt();
+        int n = in.nextInt();
+        int[] apple = new int[m];
+        for(int apple_i=0; apple_i < m; apple_i++){
+            apple[apple_i] = in.nextInt();
+        }
+        int[] orange = new int[n];
+        for(int orange_i=0; orange_i < n; orange_i++){
+            orange[orange_i] = in.nextInt();
+        }
+        printNumFruits(apple,orange);
     }
 }
 
