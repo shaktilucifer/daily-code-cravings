a5d3cd0f987c9f8d3df03c399100bea6f89b9718
Shakti <shakticse@gmail.com>
2017-07-18 23:18:02 -0400

    remove scan

--- puzzles/hrank_algo/DiagonalDifference.java
+++ puzzles/hrank_algo/DiagonalDifference.java
@@ -8,13 +8,12 @@ public class DiagonalDifference {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int a[][] = new int[n][n];
-        for(int a_i=0; a_i < n; a_i++){
-            for(int a_j=0; a_j < n; a_j++){
-                a[a_i][a_j] = in.nextInt();
-            }
-        }
+        int n = 3;
+        int a[][] = new int[][]{
+              {11, 2, 4 },
+              { 4, 5, 6 },
+              { 10, 8, -12},
+        };
         int sumDiag1 = 0;
         int sumDiag2 = 0;
         for(int i=0; i < n; i++){
