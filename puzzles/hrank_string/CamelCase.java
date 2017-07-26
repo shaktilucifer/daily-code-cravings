17eb4dc75e6c33a417c7bc0d1036e5644ccc0fe4
Shakti <shakticse@gmail.com>
2017-07-25 21:56:30 -0400

    remove scanner

--- puzzles/hrank_string/CamelCase.java
+++ puzzles/hrank_string/CamelCase.java
@@ -3,8 +3,7 @@ import java.io.*;
 public class CamelCase {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String s = in.next();
+        String s = "saveChangesInTheEditor";
         int words = 1;
         for(int i=0; i< s.length(); i++){
             if(s.charAt(i) >=65 && s.charAt(i) <= 90){
