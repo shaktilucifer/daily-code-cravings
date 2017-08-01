0c5ded71ffd95230b7fbf77f6783ac2fd06b90b0
Shakti <shakticse@gmail.com>
2017-07-31 21:50:23 -0400

    Update RecordBreak.java

--- puzzles/hrank_algo/impl/RecordBreak.java
+++ puzzles/hrank_algo/impl/RecordBreak.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class RecordBreak {
 
-    static int[] getRecord(int[] s){
+    static void getRecord(int[] s){
         int mostPointsCounter = 0, leastPointsCounter = 0;
         int max = s[0],min = s[0];
         for(int i=1; i < s.length; i++){
@@ -14,19 +14,13 @@ public class RecordBreak {
               min = s[i];
           }
         }
-       return new int[]{mostPointsCounter,leastPointsCounter};
+        System.out.println(mostPointsCounter+" "+ leastPointsCounter);
     
     }
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] s = new int[n];
-        for(int s_i=0; s_i < n; s_i++){
-            s[s_i] = in.nextInt();
-        }
-        int[] result = getRecord(s);
-        System.out.println(result[0]+" "+ result[1]);
+        int[] s = new int[]{10 ,5 ,20 ,20 ,4 ,5 ,2 ,25 ,1};
+        printRecord(s);
     }
 }
 // Note probably try doing this with a tree.
