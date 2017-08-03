742c3c42a28c95e0b3f38c8fb10c4900caf34684
shakti lucifer <shakticse@gmail.com>
2017-08-02 21:53:08 -0400

    Cool inner iteration git add .

--- jsTemplateFun/handleBars/appHandleBars.js
+++ jsTemplateFun/handleBars/appHandleBars.js
@@ -1,13 +1,21 @@
-$(document).ready(function(){
-var source   = $("#entry-template").html();
-var template = Handlebars.compile(source);
-var peopleObject = {
-    people: [
-    "smogon",
-    "imprfectluck",
-    "lucifer"
-]};
-var hbsRenderedElement = template(peopleObject); 
-console.log(hbsRenderedElement);
-$("#peopleList").append(hbsRenderedElement);
+$(document).ready(function () {
+    var source = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+    var peopleObject = {
+        people: [
+            "smogon",
+            "imprfectluck",
+            "lucifer"
+        ],
+        nestedPeople: [
+            {
+                smogon: {
+                    firstName: "smog"
+                }
+            }
+        ]
+    };
+    var hbsRenderedElement = template(peopleObject);
+    console.log(hbsRenderedElement);
+    $("#peopleList").append(hbsRenderedElement);
 });
