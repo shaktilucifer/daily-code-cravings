b1d2c40b0d83b8f91fdaafb39bbb363ccaebf91a
shakti lucifer <shakticse@gmail.com>
2017-08-02 21:30:53 -0400

    Basic iteration - built in

--- jsTemplateFun/handleBars/appHandleBars.js
+++ jsTemplateFun/handleBars/appHandleBars.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-
+var source   = $("#entry-template").html();
+var template = Handlebars.compile(source);
+var peopleObject = {
+    people: [
+    "smogon",
+    "imprfectluck",
+    "lucifer"
+]};
+var hbsRenderedElement = template(peopleObject); 
+console.log(hbsRenderedElement);
+$("#peopleList").append(hbsRenderedElement);
 });
