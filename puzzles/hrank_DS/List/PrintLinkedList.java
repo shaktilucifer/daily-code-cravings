07ed4b4f98a901c84294652d81910fc8c363421d
Shakti <shakticse@gmail.com>
2017-08-16 22:48:28 -0400

    beautify

--- puzzles/hrank_DS/List/PrintLinkedList.java
+++ puzzles/hrank_DS/List/PrintLinkedList.java
@@ -1,13 +1,12 @@
 // Just function 
 void Print(Node head) {
-  if(head == null){
-  }else{
-      Node temp = head;
-      while(temp.next != null){
-          System.out.println(temp.data);
-          temp = temp.next;          
-      }
-      System.out.println(temp.data);
-  }
-  
+    if (head == null) {} else {
+        Node temp = head;
+        while (temp.next != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+        System.out.println(temp.data);
+    }
+
 }
