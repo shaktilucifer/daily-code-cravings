27822cb553e43a35c7c12b471e0c51e6198de7de
Shakti <shakticse@gmail.com>
2017-08-20 23:20:53 -0400

    Create InsertLinkedList.java

---
+++ puzzles/hrank_DS/List/InsertLinkedList.java
@@ -0,0 +1,24 @@
+/*
+  Insert Node at the end of a linked list 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+Node Insert(Node head,int data) {
+    Node temp = head;
+    Node nodeToInsert = new Node();
+    nodeToInsert.data = data;
+    if(head == null){
+        head = nodeToInsert;
+        return head;
+    }
+    while(temp.next != null){
+        temp = temp.next;
+    }
+    temp.next = nodeToInsert;
+    return head;
+  
+}
